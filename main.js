e76a1b1c8a183ff67d5da180eb4c8b9ecf49e94d
jiageng <jiageng95@163.com>
2020-07-22 01:00:32 +0800

    update: .gitignore

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ const msgPath = process.env.GIT_PARAMS
 const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim()
+const commitRE = /^(revert: )?(feat|fix|update|release|docs|style|refactor|build)(\(.+\))?: .{1,50}/
 chalk.level = 1
-const commitRE = /^(revert: )?(feat|fix|update|release|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(\(.+\))?: .{1,50}/
 if (!commitRE.test(msg)) {
   console.log()
   console.log(`${chalk.bgRed.white(' ERROR ')} ${chalk.red(
     `invalid commit message format.`
   )}\n\n` + chalk.red(
     `Please submit the correct message format. Examples:\n\n`) +
-    `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-    chalk.red(`See .github/commit-convention.md for more details.\n`)
+    `    ${chalk.green(`feat: 添加 /views/login`)}\n` +
+    `    or\n` +
+    `    ${chalk.green(`fix: 修复 xxxBUG`)}\n` +
+    `    or\n` +
+    `    ${chalk.green(`update: 修改 /views/login`)}\n\n` +
+    chalk.red(`See https://shimo.im/docs/uJRXiSTYx2UhfVG5/  for more details.\n`)
   )
+  process.exit(1)
 }

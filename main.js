7fcc9acf016c3f4cb18febd94e404a004b98bea6
jiageng <jiageng95@163.com>
2020-07-22 00:03:11 +0800

    update: 32

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const commitRE = /^(revert: )?(feat|fix|update|release|docs|dx|style|refactor|pe
 if (!commitRE.test(msg)) {
   console.log()
   console.log(`${chalk.bgRed.white(' ERROR ')} ${chalk.red(
-    `提交不规范, 亲人两行泪!\n\n`
-  )}`)
+    `invalid commit message format.`
+  )}\n\n` + chalk.red(
+    `Please submit the correct message format. Examples:\n\n`) +
+    `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+    chalk.red(`  See .github/commit-convention.md for more details.\n`)
+  )
 }

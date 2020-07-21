06e565a3d486d87987c5374aef3049b6fd611142
jiageng <jiageng95@163.com>
2020-07-21 23:26:03 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const chalk = require('chalk')
-// console.log(122111)
+console.log(chalk.red(
+    `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+  ))
 console.error(chalk.bgRed.white(' ERROR '))

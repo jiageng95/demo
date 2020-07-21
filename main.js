fc1be867dbb368ab46d7526891057da57cfce1e1
jiageng <jiageng95@163.com>
2020-07-21 23:33:11 +0800

    update232

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const chalk = require('chalk')
 const msgPath = process.env.GIT_PARAMS
 const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
+  .trim()
 console.log(msgPath, msg)
 console.log(chalk.red(
-    `  1Proper commit message format is required for automated changelog generation. Examples:\n\n`
+    `  21Proper commit message format is required for automated changelog generation. Examples:\n\n`
 ))
 console.error(chalk.bgRed.white(' ERROR '))

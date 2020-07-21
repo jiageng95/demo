9a46bcda29fc31b07a4c7149a648c49f6aba63e4
jiageng <jiageng95@163.com>
2020-07-21 23:36:43 +0800

    update232

--- main.js
+++ main.js
@@ -5,14 +5,9 @@ const msg = require('fs')
   .trim()
 chalk.level = 1
 const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
-if (commitRE.test(msg)) {
+if (!commitRE.test(msg)) {
   console.log()
   console.log(`${chalk.bgRed.white(' ERROR ')} ${chalk.red(
     `提交不规范, 亲人两行泪\n\n`
   )}`)
 }
-console.log(msgPath, msg)
-console.log(chalk.red(
-    `  21Proper commit message format is required for automated changelog generation. Examples:\n\n`
-))
-console.error(chalk.bgRed.white(' ERROR '))

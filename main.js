47d64824a509b696c367f822bc7cc18307784024
jiageng <jiageng95@163.com>
2020-07-21 23:41:57 +0800

    update 32

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim()
 chalk.level = 1
-const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|update|release|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(\(.+\))?: .{1,50}/
 if (!commitRE.test(msg)) {
   console.log()
   console.log(`${chalk.bgRed.white(' ERROR ')} ${chalk.red(

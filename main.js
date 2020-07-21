82b75b6e85cec02d87f12cb0f6c8a583e106bc74
jiageng <jiageng95@163.com>
2020-07-21 23:36:29 +0800

    update232

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const chalk = require('chalk')
 const msgPath = process.env.GIT_PARAMS
 const msg = require('fs')
-.readFileSync(msgPath, 'utf-8')
-.trim()
-chalk.enabled = true
+  .readFileSync(msgPath, 'utf-8')
+  .trim()
 chalk.level = 1
+const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+if (commitRE.test(msg)) {
+  console.log()
+  console.log(`${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+    `提交不规范, 亲人两行泪\n\n`
+  )}`)
+}
 console.log(msgPath, msg)
 console.log(chalk.red(
     `  21Proper commit message format is required for automated changelog generation. Examples:\n\n`
